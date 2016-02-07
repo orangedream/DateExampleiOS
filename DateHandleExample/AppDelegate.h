14992d38ece11be77270cadd0a293ffe9e29f0ad
Orange Chang <orange@iotec.tw>
2016-02-07 10:20:14 +0800

    Initial push

---
+++ DateHandleExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DateHandleExample
+//
+//  Created by Orange Chang on 2016/2/7.
+//  Copyright © 2016年 iotec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
